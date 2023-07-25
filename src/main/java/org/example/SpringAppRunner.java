5547f70c9728b1493f4112e8c119ce1765966b80
yolkaSD <yolka.sd@gmail.com>
2023-07-25 22:36:10 +0700

    refactoring SpringAppRunner

--- src/main/java/org/example/SpringAppRunner.java
+++ src/main/java/org/example/SpringAppRunner.java
@@ -1,23 +1,28 @@
 package org.example;
 import org.example.model.passwordGenerator.PasswordGenerator;
 import org.example.configs.passwordConfig.builder.PasswordConfigBuilder;
-import org.example.configs.springconfig.PasswordGeneratorConfig;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
-public class Main {
+@Configuration
+@ComponentScan("org.example")
+@PropertySource("classpath:password.properties")
+public class SpringAppRunner {
     public static void main(String[] args) {
-        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(PasswordGeneratorConfig.class);
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(SpringAppRunner.class);
         context.getBean("passwordConfigBuilder", PasswordConfigBuilder.class)
                 .setLowerCaseLetters(true)
                 .setNumbers(true)
                 .setUpperCaseLetters(true)
                 .setSpecialSymbol(true)
-                .setPassLength(20)
+                .setPassLength(1)
                 .build();
 
 
         PasswordGenerator passwordGenerator = context.getBean("passwordGenerator", PasswordGenerator.class);
-        for (int i = 0; i < 100; i++) {
+        for (int i = 0; i < 10; i++) {
             System.out.println(passwordGenerator.createPassword());
         }
     }
